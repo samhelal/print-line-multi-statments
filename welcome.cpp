54f1a977e7a110aca49b7c0170ff1c3f39d762e1
samhelal <93845103+samhelal@users.noreply.github.com>
2021-11-08 17:53:50 +0200

    Update welcome.cpp

--- welcome.cpp
+++ welcome.cpp
@@ -1,10 +1,9 @@
-// program printing a line of text
+// program printing a line of text with multi statments
 # include <iostream>
 
 int main() {
 
     std::cout << "\n Welcome " ;
     std::cout << " to C++!! \n\n" ;
-
 
 }
